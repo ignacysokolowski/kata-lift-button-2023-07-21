f5014584c927781fac7ab58e54c5d87fce71ad85
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2023-07-21 13:33:29 +0200

    Extract field for the button in tests

--- src/test/kotlin/liftbutton/LiftButtonTest.kt
+++ src/test/kotlin/liftbutton/LiftButtonTest.kt
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class LiftButtonTest {
+    private val button = LiftButton()
 
     @Test fun `turns the lamp on when pressed`() {
-        val button = LiftButton()
         button.press()
         assertTrue(button.isLit)
     }
